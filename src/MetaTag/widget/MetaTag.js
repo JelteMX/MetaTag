08db112c0cfbd1392bef0e2c0f980104aa92d5fc
Jelte Lagendijk <JelteMX@users.noreply.github.com>
2017-06-01 10:26:34 +0200

    Update MetaTag.js

--- src/MetaTag/widget/MetaTag.js
+++ src/MetaTag/widget/MetaTag.js
@@ -1,32 +1,13 @@
 define([
     "dojo/_base/declare",
-    "mxui/widget/_WidgetBase",
-
-    "mxui/dom",
-    "dojo/dom",
-    "dojo/dom-prop",
-    "dojo/dom-geometry",
-    "dojo/dom-class",
-    "dojo/dom-style",
-    "dojo/dom-construct",
-    "dojo/_base/array",
-    "dojo/_base/lang",
-    "dojo/text",
-    "dojo/html",
-    "dojo/_base/event",
-
-
-], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent) {
+    "mxui/widget/_WidgetBase"
+], function (declare, _WidgetBase) {
     "use strict";
 
     return declare("MetaTag.widget.MetaTag", [ _WidgetBase ], {
 
         // Variables within this widget
         arrNewElements: null,
-
-        constructor: function () {
-
-        },
 
         postCreate: function () {
             logger.debug(this.id + ".postCreate");
@@ -39,62 +20,25 @@ define([
             // Process the open graph data specific tags and create the meta elements
             this._createMetaElements(this.ogData, true);
 
-
-
             logger.debug(this.arrNewElements);
         },
 
         update: function (obj, callback) {
             logger.debug(this.id + ".update");
-
-            this._contextObj = obj;
-            this._updateRendering(callback);
+            this._executeCallback(callback, "update");
         },
 
         resize: function (box) {
-          logger.debug(this.id + ".resize");
+            // Keep this in, does not do anything
         },
 
         uninitialize: function () {
-          logger.debug(this.id + ".uninitialize");
-          logger.debug("remove meta elements: " + this.arrNewElements.length);
+            logger.debug(this.id + ".uninitialize");
+            logger.debug("remove meta elements: " + this.arrNewElements.length);
 
-          for (var j = 0; j < this.arrNewElements.length; j++) {
-            document.head.removeChild(this.arrNewElements[j]);
-          } 
-        },
-
-        _updateRendering: function (callback) {
-            logger.debug(this.id + "._updateRendering");
-
-            if (this._contextObj !== null) {
-                dojoStyle.set(this.domNode, "display", "block");
-            } else {
-                dojoStyle.set(this.domNode, "display", "none");
-            }
-
-            this._executeCallback(callback, "_updateRendering");
-        },
-
-        // Shorthand for running a microflow
-        _execMf: function (mf, guid, cb) {
-            logger.debug(this.id + "._execMf");
-            if (mf && guid) {
-                mx.ui.action(mf, {
-                    params: {
-                        applyto: "selection",
-                        guids: [guid]
-                    },
-                    callback: lang.hitch(this, function (objs) {
-                        if (cb && typeof cb === "function") {
-                            cb(objs);
-                        }
-                    }),
-                    error: function (error) {
-                        console.debug(error.description);
-                    }
-                }, this);
-            }
+            for (var j = 0; j < this.arrNewElements.length; j++) {
+                document.head.removeChild(this.arrNewElements[j]);
+            } 
         },
 
         // Shorthand for executing a callback, adds logging to your inspector
